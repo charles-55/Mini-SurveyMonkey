a9e2732bc44219d24a8839924661f0ab77a67a0b
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-11-01 15:31:50 -0400

    initialized project

---
+++ src/main/java/sysc4806/group27/minisurveymonkey/MiniSurveyMonkeyApplication.java
@@ -0,0 +1,13 @@
+package sysc4806.group27.minisurveymonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSurveyMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSurveyMonkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sysc4806/group27/minisurveymonkey/MiniSurveyMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package sysc4806.group27.minisurveymonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSurveyMonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
