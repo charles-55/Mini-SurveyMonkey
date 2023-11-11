c661c09f67fc6d456c1e8cec3f225bc7c19a32f2
osasiyamu <140278554+osasiyamu@users.noreply.github.com>
2023-11-11 14:39:17 -0500

    Basic Structure of result and Number question.

--- src/main/java/sysc4806/group27/minisurveymonkey/controller/ViewController.java
+++ src/main/java/sysc4806/group27/minisurveymonkey/controller/ViewController.java
@@ -1,4 +1,17 @@
 package sysc4806.group27.minisurveymonkey.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import sysc4806.group27.minisurveymonkey.model.Survey;
+
 public class ViewController {
+
+    @GetMapping("/results/{id}")
+    public String getSurveyResults(@PathVariable("id") Long ID, Model model){
+        Survey survey = new Survey(); //needs to be changed to get the autowired survey
+        model.addAttribute("ID", ID);
+        model.addAttribute("survey", survey);
+        return "result";
+    }
 }
