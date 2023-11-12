054c65fb9b191fe217b9d0e063140b4b8195f758
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-11-12 13:47:23 -0500

    testing yml

--- src/test/java/sysc4806/group27/minisurveymonkey/MiniSurveyMonkeyApplicationTests.java
+++ src/test/java/sysc4806/group27/minisurveymonkey/MiniSurveyMonkeyApplicationTests.java
@@ -3,7 +3,7 @@ package sysc4806.group27.minisurveymonkey;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {MiniSurveyMonkeyApplication.class})
 class MiniSurveyMonkeyApplicationTests {
 
     @Test
