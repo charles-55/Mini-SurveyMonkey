db059c2bcf885e0e69310767229acc853b5c96ea
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-11-12 15:31:42 -0500

    testing yml

---
+++ src/test/java/sysc4806/group27/minisurveymonkey/SurveyTests.java
@@ -0,0 +1,36 @@
+package sysc4806.group27.minisurveymonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import sysc4806.group27.minisurveymonkey.model.Survey;
+import sysc4806.group27.minisurveymonkey.repository.SurveyRepository;
+import sysc4806.group27.minisurveymonkey.service.SurveyService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = {MiniSurveyMonkeyApplication.class})
+@AutoConfigureMockMvc
+public class SurveyTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private SurveyRepository surveyRepository;
+
+    @Test
+    public void allSurveys() throws Exception {
+        this.mockMvc.perform(get("/survey/all")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void showSurveys() throws Exception {
+        for(Survey survey : surveyRepository.findAll())
+            this.mockMvc.perform(post("/survey/" + survey.getName())).andDo(print()).andExpect(status().isOk());
+    }
+}
