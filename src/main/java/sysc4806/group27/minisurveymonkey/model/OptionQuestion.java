bea84d470c9e6854ec7f79e26e59d0b717d7ccce
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-11-12 23:01:38 -0500

    trying to merge to pr #25

--- src/main/java/sysc4806/group27/minisurveymonkey/model/OptionQuestion.java
+++ src/main/java/sysc4806/group27/minisurveymonkey/model/OptionQuestion.java
@@ -1,4 +1,19 @@
 package sysc4806.group27.minisurveymonkey.model;
 
+import jakarta.persistence.Entity;
+
+import java.util.ArrayList;
+
+@Entity
 public class OptionQuestion extends Question {
+    private ArrayList<String> answers;
+
+    public void addAnswer(String answer) {
+        answers.add(answer);
+    }
+
+    @Override
+    public Object getAnswers() {
+        return null;
+    }
 }
