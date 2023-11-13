1cc35afb6eb001bd044582dd6bf5ff475c9d0c79
osasiyamu <140278554+osasiyamu@users.noreply.github.com>
2023-11-12 22:42:44 -0500

    Result.html and Result Controller fully implemented

--- src/main/java/sysc4806/group27/minisurveymonkey/controller/ResultController.java
+++ src/main/java/sysc4806/group27/minisurveymonkey/controller/ResultController.java
@@ -5,16 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import sysc4806.group27.minisurveymonkey.model.Survey;
+import sysc4806.group27.minisurveymonkey.model.TextQuestion;
+import sysc4806.group27.minisurveymonkey.service.SurveyService;
 
 @Controller
 public class ResultController {
 
+    private final SurveyService surveyService;
+
+    public ResultController(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
 
     @GetMapping("/results/{name}")
     public String getSurveyResults(@PathVariable("name") String name, Model model){
-        Survey survey = new Survey(); //needs to be changed to get the autowired survey
-        model.addAttribute("ID", name);
-        model.addAttribute("survey", survey);
+        model.addAttribute("survey", surveyService.getSurvey(name));
         return "result";
     }
 }
