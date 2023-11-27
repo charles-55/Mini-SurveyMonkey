0703b2ba059287e29809e8db57bcf978fac62c33
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-11-27 00:23:55 -0500

    fixed show survey error

---
+++ src/test/java/sysc4806/group27/minisurveymonkey/MiniSurveyMonkeyTest.java
@@ -0,0 +1,15 @@
+//package sysc4806.group27.minisurveymonkey;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@SpringBootTest(classes = {MiniSurveyMonkeyApplication.class})
+//@AutoConfigureMockMvc
+//public class MiniSurveyMonkeyTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//}
